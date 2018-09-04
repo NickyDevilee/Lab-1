7053d81031d68e3fe562a5daece968d9cc10ea92
NickyDevilee <99047338@mydavinci.nl>
2018-09-04 11:25:42 +0200

    bestanden beter georganiseerd

---
+++ lab 1.js
@@ -0,0 +1,7 @@
+/*Nicky Devilee
+Bol4 applicatieontwikkelaar*/
+alert('We gaan 3 vragen stellen'); // geeft popup voor "We gaan 3 vragen stellen"
+document.write('Uw voornaam is: ' + prompt('Voer je voornaam in') + '<br>'); // vraagt om je voornaam en geeft het antwoord weer op de site
+document.write('Uw achternaam is: ' + prompt('Voer je achternaam in') + '<br>'); // vraagt om je achternaam en geeft het antwoord weer op de site
+document.write('Uw leeftijd is: ' + prompt('Voer je leeftijd in') + '<br>'); // vraagt om je leeftijd en geeft het antwoord weer op de site
+document.write("Bedankt voor het beantwoorden van de vragen!"); // schrijft "Bedankt voor het beantwoorden van de vragen!" op de site
